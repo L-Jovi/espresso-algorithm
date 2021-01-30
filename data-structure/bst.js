d837a5c8edc0a21886c3f14de08b24568cc73c69
Eternal_Jovi <eternal.l.jovi@gmail.com>
2021-01-30 12:09:58 +0800

    feat: review leetcode 1st

--- data-structure/bst.js
+++ data-structure/bst.js
@@ -25,32 +25,32 @@ class BST {
       this.root = new Node(data)
       return
 
-    } else {
-      const searchTree = function(node) {
-        if (data < node.data) {
-          if (node.left === null) {
-            node.left = new Node(data)
-            return
-
-          } else if (node.left !== null) {
-            return searchTree(node.left)
-          }
-
-        } else if (data > node.data) {
-          if (node.right === null) {
-            node.right = new Node(data)
-            return
-          } else if (node.right !== null) {
-            return searchTree(node.right)
-          }
-
-        } else {
-          return null
-        }
-      }
-
-      return searchTree(node)
     }
+
+    const searchTree = function(node) {
+      if (data < node.data) {
+        if (node.left === null) {
+          node.left = new Node(data)
+          return
+
+        } else if (node.left !== null) {
+          return searchTree(node.left)
+        }
+
+      } else if (data > node.data) {
+        if (node.right === null) {
+          node.right = new Node(data)
+          return
+        } else if (node.right !== null) {
+          return searchTree(node.right)
+        }
+
+      } else {
+        return null
+      }
+    }
+
+    return searchTree(node)
   }
 
   findMin() {
