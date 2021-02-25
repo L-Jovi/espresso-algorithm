d68a4e5d687210834d479fbcfce597e75d594b86
Eternal_Jovi <eternal.l.jovi@gmail.com>
2021-02-25 20:14:27 +0800

    feat: add leetcode 114

--- leetcode/114-flatten-binary-tree-to-linked-list/index.js
+++ leetcode/114-flatten-binary-tree-to-linked-list/index.js
@@ -10,7 +10,25 @@
 /**
  * @param {TreeNode} root
  * @return {void} Do not return anything, modify root in-place instead.
+ * https://labuladong.github.io/algo/%E6%95%B0%E6%8D%AE%E7%BB%93%E6%9E%84%E7%B3%BB%E5%88%97/%E4%BA%8C%E5%8F%89%E6%A0%91%E7%B3%BB%E5%88%971.html
  */
 const flatten = (root) => {
+  if (!root) {
+    return
+  }
 
+  flatten(root.left)
+  flatten(root.right)
+
+  const left = root.left
+  const right = root.right
+
+  root.left = null
+  root.right = left
+
+  let node = root
+  while (node.right) {
+    node = node.right
+  }
+  node.right = right
 }
