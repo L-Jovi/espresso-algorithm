b0a5b690f616c89f23f1dad6dd3cf08014ad720b
Eternal_Jovi <eternal.l.jovi@gmail.com>
2021-06-07 16:55:09 +0800

    feat: add leetcode 654

--- leetcode/654-maximum-binary-tree/index.js
+++ leetcode/654-maximum-binary-tree/index.js
@@ -11,12 +11,18 @@
  * @return {TreeNode}
  */
 const constructMaximumBinaryTree = (nums) => {
+  if (nums.length === 0) {
+    return null
+  }
+
   // find max value
   const root = {}
+  const max = Math.max(...nums)
   root.val = max
+  const indexMax = nums.findIndex(v => v === max)
 
-  const leftItems = []
-  const rightItems = []
+  const leftItems = nums.slice(0, indexMax)
+  const rightItems = nums.slice(indexMax + 1, nums.length)
 
   root.left = constructMaximumBinaryTree(leftItems)
   root.right = constructMaximumBinaryTree(rightItems)
